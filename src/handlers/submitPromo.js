952c574affcac1cc2b2d654b6211f46e8b7b330f
ShotsxFired <shotsxxxfired@gmail.com>
2024-05-07 13:43:36 -0600

    fixed issues with Submit Promo

--- src/handlers/submitPromo.js
+++ src/handlers/submitPromo.js
@@ -4,23 +4,48 @@ import { get } from "../utils/get.js"
 
 module.exports.handler = async (event) => {
     try {
-        const { eventID, walletID, code, promoTickets, promoMulti } = JSON.parse(event.body)
+        const { eventID, walletID, code } = JSON.parse(event.body)
 
         //Configuring this fields needed for the request is consolidated into the loadVariables method.
-        const { keyPromo, keyEntry, tableName, condExpress, promoEntryItem, keyPromoEntry } = await loadVairables(eventID, walletID, code, promoTickets, promoMulti)
+        const { keyPromo, keyEntry, tableName, condExpress, promoEntryItem, keyPromoEntry } = await loadVairables(eventID, walletID, code)
         // get client
         const client = await getClient()
 
         //check to see if there is already a promo entry for this event/user
         const { promoEntryExists, codes, codeAlreadyRedeemed } = await peExists(eventID, walletID, code, client)
+
         //transaction input
         let input = {}
 
         if (codeAlreadyRedeemed) {
+            console.log("Code has already been used for this Event")
             const response = {
                 statusCode: 400,
                 body: JSON.stringify({
-                    error: `User has aleady redeemed the code: ${code} for this event.`
+                    error: `User has aleady redeemed the code "${code}" for this event.`
+                })
+            }
+
+            console.log(response)
+            return response
+        }
+
+        const { promoTickets, promoMulti, deadline } = await getPromoData(eventID, code)
+
+        if (deadline < Date.now()) {
+            const response = {
+                statusCode: 400,
+                body: JSON.stringify({
+                    error: `Deadline for code "${code}" has passed!`
+                })
+            }
+            return response
+        }
+        if (!promoTickets || !promoMulti) {
+            const response = {
+                statusCode: 400,
+                body: JSON.stringify({
+                    error: `Submitted code "${code}" does not exist for this event!`
                 })
             }
             return response
@@ -47,7 +72,6 @@ module.exports.handler = async (event) => {
                                 ":inc": { "N": "1" },
                                 ":val": { "N": "0" }
                             },
-                            ReturnValues: "ALL_NEW",
                         }
                     },
                     {
@@ -134,7 +158,7 @@ module.exports.handler = async (event) => {
 }
 
 const peExists = async (eventID, walletID, code) => {
-    const pk = `PE#${eventID.split("#")[1]}`
+    const pk = `PE#${eventID}`
     const sk = `PE#${walletID}`
 
     const tableName = process.env.TABLE_NAME
@@ -142,36 +166,51 @@ const peExists = async (eventID, walletID, code) => {
     let promoEntryExists = false
     let codes = []
     let codeAlreadyRedeemed = false
-
     const res = await get(tableName, key)
 
     // Promo Exists
     if (res.Item) {
         promoEntryExists = true
-        codes = Array.from(Item.Codes)
+        codes = Array.from(res.Item.Codes)
     }
     codeAlreadyRedeemed = codes.includes(code)
     codes.push(code)
 
     return { promoEntryExists, codes, codeAlreadyRedeemed }
 }
+const getPromoData = async (eventID, code) => {
+    const pk = `PROMO#${eventID}`
+    const sk = `PROMO#${code}`
+
+    const tableName = process.env.TABLE_NAME
+    const key = { PK: pk, SK: sk }
+    const res = await get(tableName, key)
+    if (res.Item) {
+        let promoTickets = res.Item.TicketsBase
+        let promoMulti = res.Item.TicketsMulti
+        let deadline = res.Item.Deadline
+
+        return { promoTickets, promoMulti, deadline }
+    }
+    return (false, false, 0)
+}
 
 const loadVairables = async (eventID, walletID, code) => {
     let codes = [code]
 
     //PROMO ENTRY
-    const pkPromoEntry = `PE#${eventID.split("#")[1]}`
+    const pkPromoEntry = `PE#${eventID}`
     const skPromoEntry = `PE#${walletID}`
     const keyPromoEntry = { PK: { "S": pkPromoEntry }, SK: { "S": skPromoEntry } }
     const promoEntryItem = { PK: { "S": pkPromoEntry }, SK: { "S": skPromoEntry }, Codes: { "SS": codes } }
 
     //PROMO
-    const pkPromo = `PROMO#${eventID.split("#")[1]}`
+    const pkPromo = `PROMO#${eventID}`
     const skPromo = `PROMO#${code}`
     const keyPromo = { PK: { "S": pkPromo }, SK: { "S": skPromo } }
 
     //ENTRY
-    const pkEntry = `ENTRY#${eventID.split("#")[1]}`
+    const pkEntry = `ENTRY#${eventID}`
     const skEntry = `USER#${walletID}`
     const keyEntry = { PK: { "S": pkEntry }, SK: { "S": skEntry } }
 
