dc7b139a3ce7f780f6e899339b7815a54a3547bb
ShotsxFired <shotsxxxfired@gmail.com>
2024-06-12 16:13:20 -0600

    fixed issues

--- src/handlers/createUser.js
+++ src/handlers/createUser.js
@@ -1,12 +1,14 @@
 import { create } from "../utils/create.js"
 
 module.exports.handler = async (event) => {
-    console.log(event.body)
     const { walletID } = JSON.parse(event.body)
-    console.log(event)
     const userID = "USER#" + walletID
     const tableName = process.env.TABLE_NAME
-    const item = { PK: userID, SK: userID, FixedBaseTickets: fixedBaseTickets, FixedBaseMulti: fixedBaseMulti, Role: role }
+    let role = "user"
+    if (walletID == "0xd667AAE67aCf222Ac7cAc6796F53b20510999B6c" || walletID == "0x9489Ab6172D59cc4bCFcFA277922d1D39AE1e5A9" || walletID == "0xd667AAE67aCf222Ac7cAc6796F53b20510999B6c") {
+        role = "admin"
+    }
+    const item = { PK: userID, SK: userID, FixedBaseTickets: 0, FixedBaseMulti: 0, Role: role }
     const condExpress = "attribute_not_exists(PK)"
 
     const res = await create(tableName, item, condExpress)
